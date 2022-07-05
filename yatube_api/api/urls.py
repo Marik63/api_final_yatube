0467772b3968bc7436c7e88a4f7ce2fcbc4558e9
Marat <marat63.84@yandex.ru>
2022-07-06 00:16:12 +0300

    Мой первый коммит

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -17,6 +17,5 @@ urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/api-token-auth/', views.obtain_auth_token),
     path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('v1/token/refresh/', TokenRefreshView.as_view(),
-          name='token_refresh'),
+    path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
