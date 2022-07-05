1a4e1719589e47a4c02964e40517a57156c51b62
Marat <marat63.84@yandex.ru>
2022-07-05 23:34:36 +0300

    Мой первый коммит

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -8,12 +8,12 @@ from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
 router.register('follow', FollowViewSet, basename='follow')
-router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
+router.register(r'posts/(?P<post_id>\\d+)/comments', CommentViewSet,
                 basename='comments')
 router.register('groups', GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('v1/', include(router.urls)),
     path('v1/auth/', include('djoser.urls')),
     path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('v1/token/refresh/', TokenRefreshView.as_view, name='token_refresh')
