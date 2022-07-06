8a254263dbe1e2678002894c72e06f1c77412c87
Marat <marat63.84@yandex.ru>
2022-07-06 11:21:23 +0300

    Мой первый коммит

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
@@ -14,4 +16,7 @@ urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
+    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
 ]
