fe00f036b059881e44fb043ce6af38650114f44c
Marat <marat63.84@yandex.ru>
2022-07-06 11:41:48 +0300

    Мой первый коммит

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -4,6 +4,7 @@ from posts.models import Group, Post, User
 from rest_framework import filters, permissions, status, viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
+from rest_framework.pagination import LimitOffsetPagination
 
 from .permission import IsOwnerOrReadOnly
 from .serializers import (
@@ -14,6 +15,7 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    pagination_class = LimitOffsetPagination
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['group']
 
