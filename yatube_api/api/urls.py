09c9237232c785dea67075978ca32803c537f7a7
Marat <marat63.84@yandex.ru>
2022-07-06 11:18:40 +0300

    Мой первый коммит

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,7 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
 
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
@@ -14,9 +12,6 @@ router.register('groups', GroupViewSet, basename='groups')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/auth/', include('djoser.urls')),
-    path('v1/auth/', include('djoser.urls.jwt')),
-    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('v1/token/refresh/', TokenRefreshView.as_view(),
-         name='token_refresh'),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -1,4 +1,5 @@
 import os
+
 from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
