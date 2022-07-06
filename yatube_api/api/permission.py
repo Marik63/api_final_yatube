27c5d49e7ef856c90b98b30917a63266c903bafd
Marat <marat63.84@yandex.ru>
2022-07-06 12:34:33 +0300

    Мой первый коммит

--- yatube_api/api/permission.py
+++ yatube_api/api/permission.py
@@ -2,11 +2,5 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class IsOwnerOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.method in SAFE_METHODS
-            or request.user.is_authenticated
-        )
-
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        return request.method in SAFE_METHODS or obj.author == request.user
