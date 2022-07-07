f44e60869cd6a1f73a89b4c486a284d1751459ce
Marat <marat63.84@yandex.ru>
2022-07-07 20:14:17 +0300

    Исправил по PEP8

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from posts.models import Comment, Follow, Group, Post, User
 
+
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True,
