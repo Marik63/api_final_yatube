45386e5e02207af7fb2b8ab116ebd283bfca1d20
Marat <marat63.84@yandex.ru>
2022-07-08 19:39:27 +0300

    Исправил методы в файле views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, permissions, status, viewsets
-from rest_framework.response import Response
+from rest_framework import filters, permissions, viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.pagination import LimitOffsetPagination
 
